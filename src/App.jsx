e594794f620f4443e0bd69ab68eba5051facd007
Steven Wilson <stevenwilson@gsjproducts.co.uk>
2026-09-20 14:42:03 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,30 @@ import { UserProvider } from "./context/UserContext";
 import Home from "./pages/home/home";
 import Profile from "./pages/profile/profile";
 
+// NEW: import the real Account page
+import Account from "./pages/profile/account";
+
 export default function App() {
   return (
     <UserProvider>
       <BrowserRouter>
         <Routes>
+
+          {/* Default redirect */}
           <Route path="/" element={<Navigate to="/home" replace />} />
+
+          {/* Main screens */}
           <Route path="/home" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
+
+          {/* Profile sub-pages */}
+          <Route path="/profile/account" element={<Account />} />
+          <Route path="/profile/subscription" element={<div>Subscription Page</div>} />
+          <Route path="/profile/dj" element={<div>DJ Settings Page</div>} />
+          <Route path="/profile/app" element={<div>App Settings Page</div>} />
+
+          {/* Catch-all */}
           <Route path="*" element={<Navigate to="/home" replace />} />
-          <Route path="/profile/account" element={<div>Account Page</div>} />
-<Route path="/profile/subscription" element={<div>Subscription Page</div>} />
-<Route path="/profile/dj" element={<div>DJ Settings Page</div>} />
-<Route path="/profile/app" element={<div>App Settings Page</div>} />
 
         </Routes>
       </BrowserRouter>
---
+++ src/pages/profile/account.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./account.css";
+
+export default function Account() {
+  return (
+    <div className="account-screen">
+
+      <h1 className="account-title">Account</h1>
+
+      <div className="account-card">
+
+        <div className="account-avatar">
+          <img
+            src="/avatar.png"
+            alt="User Avatar"
+            className="avatar-img"
+          />
+        </div>
+
+        <div className="account-field">
+          <label>Name</label>
+          <input type="text" placeholder="Steven Wilson" />
+        </div>
+
+        <div className="account-field">
+          <label>Email</label>
+          <input type="text" placeholder="steven@example.com" />
+        </div>
+
+        <button className="account-save">
+          Save Changes
+        </button>
+
+      </div>
+    </div>
+  );
+}
